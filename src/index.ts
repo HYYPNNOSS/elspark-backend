b492407b02877e4fab3633261d16cbe41b7ab878
Hachim E. <azert9412@gmail.com>
2025-10-11 17:48:25 +0000

    hey

--- src/index.ts
+++ src/index.ts
@@ -50,6 +50,7 @@ const server = http.createServer(app);
 
 const allowedOrigins = [
   "https://elspark-frontend.vercel.app",
+  "https://elspark.online",
   "http://localhost:3000",
   "http://192.168.1.4:3000",
   "http://192.168.1.5:3000",
