3b43309b1d44f3e3875aeade9d4078f860602e2a
HYYPNNOSS <azert9412@gmail.com>
2025-10-28 18:11:09 +0100

    hji

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -7,7 +7,8 @@ export const verifyToken = (req: any, res: Response, next: NextFunction) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ error: "No token provided" });
+    res.status(401).json({ error: "No token provided" });
+    return
   }
 
   try {
@@ -26,7 +27,8 @@ export const verifyToken = (req: any, res: Response, next: NextFunction) => {
     
     next();
   } catch (err) {
-    return res.status(401).json({ error: "Invalid or expired token" });
+    res.status(401).json({ error: "Invalid or expired token" });
+    return
   }
 };
 
